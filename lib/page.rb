8f46fc6b2d24ac06ee50cfdc7e2aa9ae2d898021
gregjm <LP-pan-2>
2020-04-20 12:11:53 -0700

    Added the following:  1.) page "title" is now part of attr_reader  2.) @@urn to make the "rest/api/content" reference easier to change if ever needed.  3.) A method to return a page attachment ID

--- lib/page.rb
+++ lib/page.rb
@@ -1,11 +1,11 @@
 class PageObject < ConfluenceClient
 
-  attr_reader :id, :version, :status, :created, :last_update
+  attr_reader :title, :id, :version, :status, :created, :last_update
 
   def initialize(title, spacekey)
     @title    = title
     @spacekey = spacekey
-    @id, @version, @status, @created, @last_update, @url = get_page_info
+    @title, @id, @version, @status, @created, @last_update, @url = get_page_info
   end
 
   def url
@@ -17,7 +17,7 @@ class PageObject < ConfluenceClient
   #####################################################
   def styled_view
     begin
-      res = RestClient.get "#{@@conf_url}/rest/api/content/#{@id}", {params: {
+      res = RestClient.get "#{@@conf_url}/#{@@urn}/#{@id}", {params: {
           :expand => 'body.styled_view', :os_username => @@login, :os_password => @@pwd
       }}
     rescue RestClient::ExceptionWithResponse => e
@@ -31,7 +31,7 @@ class PageObject < ConfluenceClient
   ##################################################
   def rendered_body
     begin
-      res = RestClient.get "#{@@conf_url}/rest/api/content/#{@id}", {params: {
+      res = RestClient.get "#{@@conf_url}/#{@@urn}/#{@id}", {params: {
           :expand => 'body.view', :os_username => @@login, :os_password => @@pwd
       }}
     rescue RestClient::ExceptionWithResponse => e
@@ -42,7 +42,7 @@ class PageObject < ConfluenceClient
 
   def storage_format
     begin
-      res = RestClient.get "#{@@conf_url}/rest/api/content/#{@id}", {params: {
+      res = RestClient.get "#{@@conf_url}/#{@@urn}/#{@id}", {params: {
           :expand => 'body.storage', :os_username => @@login, :os_password => @@pwd
       }}
     rescue RestClient::ExceptionWithResponse => e
@@ -60,7 +60,7 @@ class PageObject < ConfluenceClient
           comment: 'Automated Ruby import',
           minorEdit: true
       }
-      url_mod = "#{@@conf_url}/rest/api/content/#{@id}/child/attachment?os_username=#{@@login}&os_password=#{@@pwd}"
+      url_mod = "#{@@conf_url}/#{@@urn}/#{@id}/child/attachment?os_username=#{@@login}&os_password=#{@@pwd}"
       begin
         RestClient.post(url_mod, payload, {"X-Atlassian-Token" => "nocheck"})
         true
@@ -74,6 +74,38 @@ class PageObject < ConfluenceClient
     end
   end
 
+  def delete_image(image_id)
+    begin
+      RestClient.delete "#{@@conf_url}/#{@@urn}/#{image_id}", {params: {
+          :os_username => @@login, :os_password => @@pwd
+      }}
+      # JSON.parse(response)
+    rescue RestClient::ExceptionWithResponse => e
+      puts Nokogiri.XML(e.response)
+    end
+
+  end
+
+  def image_id(page_id, image_name)
+
+    img = image_name.dup
+    img = CGI.escape(img)
+
+    begin
+      response = RestClient.get "#{@@conf_url}/#{@@urn}/#{page_id}/child/attachment", {params: {
+          :filename => img, 'os_username' => @@login, 'os_password' => @@pwd
+      }}
+
+      response = JSON.parse(response)
+      if response['results'].any?
+        return response['results'][0]['id']
+      end
+    rescue RestClient::ExceptionWithResponse => e
+      puts Nokogiri.XML(e.response)
+    end
+    nil
+  end
+
   ##################################################################
   private
   ##################################################################
@@ -81,8 +113,7 @@ class PageObject < ConfluenceClient
   # Here we can return various metadata for a given page.
   def get_page_info
     begin
-      res = RestClient.get "#{@@conf_url}/rest/api/content", {params: {
-          # :title => @title, :spaceKey => @spacekey, :os_username => @@login, :os_password => @@pwd, :expand => 'version', :expand => 'history'
+      res = RestClient.get "#{@@conf_url}/#{@@urn}", {params: {
           :title => @title, :spaceKey => @spacekey, :os_username => @@login, :os_password => @@pwd, :expand => 'version,history'
       }}
     rescue RestClient::ExceptionWithResponse => e
@@ -93,14 +124,14 @@ class PageObject < ConfluenceClient
       puts "             Page: #{title}"
       puts "             Space Key: #{spacekey}"
     end
-
-    return JSON.parse(res)['results'][0]['id'],
+    # pp JSON.parse(res)['results']
+    return JSON.parse(res)['results'][0]['title'],
+           JSON.parse(res)['results'][0]['id'],
            JSON.parse(res)['results'][0]['version']['number'],
            JSON.parse(res)['results'][0]['status'],
            JSON.parse(res)['results'][0]['history']['createdDate'],
            JSON.parse(res)['results'][0]['version']['when'],
            JSON.parse(res)['results'][0]['_links']['webui']
-
   end
 
 end
